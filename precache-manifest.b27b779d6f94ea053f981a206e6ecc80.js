f52cabeaf451448ff2a63a56bf95bb4d78547448
YuliiaChupryna <julia.chuprina2907@gmail.com>
2019-12-18 02:35:18 +0200

    Updates

---
+++ precache-manifest.b27b779d6f94ea053f981a206e6ecc80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717fb7731f65d649c5c821b9f161354d",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "b767a511cc9ac8b7527c",
+    "url": "/react_people-table/static/css/main.6e7acbd4.chunk.css"
+  },
+  {
+    "revision": "68b1658d02f8c0e09e9f",
+    "url": "/react_people-table/static/js/2.1c2721b6.chunk.js"
+  },
+  {
+    "revision": "b767a511cc9ac8b7527c",
+    "url": "/react_people-table/static/js/main.d8d6e304.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
